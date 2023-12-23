7df3f93dc4c38fcb060e06a223d19a7c9888be6b
M1racle <crazyhl@163.com>
2023-12-23 10:39:09 +0800

    ✨ 加入环境变量文件，设置页面标题

--- src/router/guard.js
+++ src/router/guard.js
@@ -1,5 +1,6 @@
 export default function registerGuard(router) {
   router.beforeEach((to, from) => {
+    document.title = import.meta.env.VITE_APP_TITLE
     // ...
     // 返回 false 以取消导航
     return true;
