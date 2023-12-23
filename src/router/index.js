25a8031dfd7695ba7e6512579ea7e09a9013f68d
M1racle <crazyhl@163.com>
2023-12-23 09:55:27 +0800

    ✨ 创建后台首页，引入 unplugin-vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
+import Home from '@/views/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
     {
       path: '/login',
       name: 'Login',
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AntdvResolver from 'antdv-component-resolver'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
     Components({
       resolvers: [AntdvResolver()],
