9455209d8c5eb08101f9820203a9362afc4008bc
M1racle <crazyhl@163.com>
2024-01-04 15:26:20 +0800

    ✨ 完善注册验证

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -6,16 +6,17 @@
                     <a-typography>
                         <a-typography-title class="text-center">注册</a-typography-title>
                     </a-typography>
-                    <a-form layout="vertical" :model="formState" name="basic" autocomplete="off" @finish="onFinish">
-                        <a-form-item label="邮箱" name="email" :rules="[{ required: true, message: '请输入邮箱!' }]">
+                    <a-form :rules="rules" layout="vertical" :model="formState" name="basic" autocomplete="off"
+                        @finish="onFinish">
+                        <a-form-item label="邮箱" name="email">
                             <a-input v-model:value="formState.email" placeholder="请输入邮箱" />
                         </a-form-item>
 
-                        <a-form-item label="密码" name="password" :rules="[{ required: true, message: '请输入密码!' }]">
+                        <a-form-item label="密码" name="password">
                             <a-input-password v-model:value="formState.password" placeholder="请输入密码" />
                         </a-form-item>
 
-                        <a-form-item label="确认密码" name="confirm_password" :rules="[{ required: true, message: '请输入密码!' }]">
+                        <a-form-item label="确认密码" name="confirm_password">
                             <a-input-password v-model:value="formState.confirm_password" placeholder="请输入密码" />
                         </a-form-item>
 
@@ -46,6 +47,72 @@ function showRegError(message) {
         description: message,
     });
 }
+
+const validatePass = async (_rule, value) => {
+    if (!value.match(/[A-Z]+/)) {
+        return Promise.reject('密码至少包含一个大写字母');
+    }
+    if (!value.match(/[a-z]+/)) {
+        return Promise.reject('密码至少包含一个小写字母');
+    }
+    if (!value.match(/[0-9]+/)) {
+        return Promise.reject('密码至少包含一个数字');
+    }
+    if (!value.match(/[-#!$@£%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]+/)) {
+        return Promise.reject('密码至少包含一个特殊符号');
+    }
+    return Promise.resolve();
+};
+
+const validateConfirmPass = async (_rule, value) => {
+    if (value !== formState.password) {
+        return Promise.reject("确认密码必须和密码相同!");
+    }
+    return Promise.resolve();
+}
+
+const rules = {
+    email: [
+        {
+            required: true,
+            message: '邮箱必须填写',
+            trigger: 'change',
+        },
+        {
+            type: 'email',
+            message: '请输入正确的邮箱',
+            trigger: 'change',
+        },
+    ],
+    password: [
+        {
+            required: true,
+            message: '请输入密码',
+            trigger: 'change',
+        },
+        {
+            min: 5,
+            max: 20,
+            message: '密码长度在5-20位',
+            trigger: 'change',
+        },
+        {
+            validator: validatePass,
+            trigger: 'change',
+        },
+    ],
+    confirm_password: [
+        {
+            required: true,
+            message: '请输入确认密码',
+            trigger: 'change',
+        },
+        {
+            validator: validateConfirmPass,
+            trigger: 'change',
+        },
+    ],
+};
 
 const onFinish = values => {
     register(values).then(resp => {
