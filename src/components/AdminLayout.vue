7e3054b9975f90fc0e1796a2e48a5f1970d78ef2
M1racle <crazyhl@163.com>
2024-01-08 11:59:30 +0800

    🎨 整理代码结构，拆分子组件

---
+++ src/components/AdminLayout.vue
@@ -0,0 +1,73 @@
+<template>
+    <a-layout style="height: 100vh;">
+        <a-layout-sider breakpoint="lg" v-model:collapsed="collapsed" :trigger="null" collapsible>
+            <div class="logo" />
+            <SliderMenus></SliderMenus>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0; border-bottom: 1px solid #f2f2f2;">
+                <a-flex justify="space-between">
+                    <div>
+                        <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
+                        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+                    </div>
+                    <div style="padding-right: 24px;">
+                        <a-dropdown>
+                            <a class="ant-dropdown-link" @click.prevent>
+                                Hover me
+                                <DownOutlined />
+                            </a>
+                            <template #overlay>
+                                <a-menu>
+                                    <a-menu-item>
+                                        <a href="javascript:;">1st menu item</a>
+                                    </a-menu-item>
+                                    <a-menu-item>
+                                        <a href="javascript:;">2nd menu item</a>
+                                    </a-menu-item>
+                                    <a-menu-item>
+                                        <a href="javascript:;">3rd menu item</a>
+                                    </a-menu-item>
+                                </a-menu>
+                            </template>
+                        </a-dropdown>
+                    </div>
+                </a-flex>
+
+            </a-layout-header>
+            <a-layout-content :style="{ margin: '0px', padding: '24px', background: '#fff', minHeight: '280px' }">
+                <slot />
+            </a-layout-content>
+        </a-layout>
+    </a-layout>
+</template>
+<script setup>
+import { MenuUnfoldOutlined, MenuFoldOutlined, DownOutlined} from '@ant-design/icons-vue';
+
+import { ref } from 'vue';
+import SliderMenus from './SliderMenus.vue';
+const collapsed = ref(false);
+</script>
+<style>
+.trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+}
+
+.trigger:hover {
+    color: #1890ff;
+}
+
+.logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 16px;
+}
+
+.site-layout .site-layout-background {
+    background: #fff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,31 @@
 <template>
-    <a-layout style="height: 100vh;">
-        <a-layout-sider breakpoint="lg" v-model:collapsed="collapsed" :trigger="null" collapsible>
-            <div class="logo" />
-            <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-                <a-menu-item key="1">
-                    <user-outlined />
-                    <span>nav 1</span>
-                </a-menu-item>
-                <a-menu-item key="2">
-                    <video-camera-outlined />
-                    <span>nav 2</span>
-                </a-menu-item>
-                <a-menu-item key="3">
-                    <upload-outlined />
-                    <span>nav 3</span>
-                </a-menu-item>
-            </a-menu>
-        </a-layout-sider>
-        <a-layout>
-            <a-layout-header style="background: #fff; padding: 0">
-                <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
-                <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
-            </a-layout-header>
-            <a-layout-content :style="{ margin: '1px', padding: '24px', background: '#fff', minHeight: '280px' }">
-                Content
+    <AdminLayout>
+        Content
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
+                Admin Home
                 Admin Home
                 <a-button type="primary" @click="logout">退出登录</a-button>
-            </a-layout-content>
-        </a-layout>
-    </a-layout>
+    </AdminLayout>
 </template>
 <script setup>
-import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons-vue';
-
-import { ref } from 'vue';
-const selectedKeys = ref(['1']);
-const collapsed = ref(false);
-
+import AdminLayout from "@/components/AdminLayout.vue"
 import { useUserStore } from '@/stores/user';
 import { useRouter } from 'vue-router';
 import { info } from '@/api/user'
@@ -55,26 +44,3 @@ info().then(resp => {
 })
 
 </script>
-<style>
-.trigger {
-    font-size: 18px;
-    line-height: 64px;
-    padding: 0 24px;
-    cursor: pointer;
-    transition: color 0.3s;
-}
-
-.trigger:hover {
-    color: #1890ff;
-}
-
-.logo {
-    height: 32px;
-    background: rgba(255, 255, 255, 0.3);
-    margin: 16px;
-}
-
-.site-layout .site-layout-background {
-    background: #fff;
-}
-</style>
